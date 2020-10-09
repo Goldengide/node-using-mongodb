936360cc4c7013e66ac2fe4f7bf45512dec991bb
Goldengide <amowogbajegideon@gmail.com>
2020-10-09 15:44:55 +0100

    module 5

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -7,7 +7,7 @@ export const addnewProduct = (req, res) => {
     let newProduct = new Product(req.body);
 
     newProduct.save((err, Product) => {
-        if(err) {
+        if (err) {
             res.send(err)
         }
         res.json(Product);
@@ -16,7 +16,7 @@ export const addnewProduct = (req, res) => {
 
 export const getProducts = (req, res) => {
     Product.find({}, (err, Product) => {
-        if(err) {
+        if (err) {
             res.send(err)
         }
         res.json(Product);
@@ -26,9 +26,28 @@ export const getProducts = (req, res) => {
 
 export const getProductWithID = (req, res) => {
     Product.findById(req.params.ProductID, (err, Product) => {
-        if(err) {
+        if (err) {
             res.send(err)
         }
         res.json(Product);
+    })
+}
+
+export const updateProduct = (req, res) => {
+    Product.findOneAndUpdate({ _id: req.params.ProductID }, req.body, { new: true, useFindAndModify: false }, (err, Product)
+        => {
+            if (err) {
+                res.send(err)
+            }
+        res.json(Product);
+    })
+}
+
+export const deleteProduct = (req, res) => {
+    Product.deleteOne({ _id: req.params.ProductID}, (err, Product) => {
+        if (err) {
+            res.send(err)
+        }
+        res.json({ message: 'successfully deleted product' });
     })
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,19 @@
-import { addnewProduct, getProducts, getProductWithID } from "../controllers/controllers";
+import { addnewProduct, deleteProduct, getProducts, getProductWithID, updateProduct } from "../controllers/controllers";
 
 const routes = (app) => {
+
     app.route('/products')
 
-        // Post endpoint
         .post(addnewProduct)
+        
         .get(getProducts);
 
     app.route('/products/:ProductID')
 
         .get(getProductWithID)
+        .put(updateProduct)
+        .delete(deleteProduct);
+
 }
 
 export default routes;
